39c019f94a7185d74f48616a92c2cbae1fad5a8e
Easit AB <github@easit.com>
2023-09-07 10:20:21 +0200

    add function Read-StringAsUTF8

---
+++ source/public/Read-StringAsUTF8.ps1
@@ -0,0 +1,48 @@
+function Read-StringAsUTF8 {
+    <#
+    .SYNOPSIS
+        Read a string and return it decoded as UTF-8.
+    .DESCRIPTION
+        *Read-StringAsUTF8* uses the UTF8Encoding class to return a string decoded in UTF8.
+        The function uses UTF8Encoding.GetBytes to encode the input string into a sequence of bytes and then UTF8Encoding.GetString to decodes the byte array into a string.
+    .EXAMPLE
+        $decodedStringInput = Read-StringAsUTF8 -InputString $StringInput
+        $exportObject = $decodedStringInput | ConvertFrom-Json
+        $EasitGOItem = $exportObject.itemToImport[0]
+        $EasitGOItem.property
+        content                                       name            rawValue
+        -------                                       ----            --------
+        Jane                                          givenName
+        jado                                          samAccountName
+        CN=Doe\, Jane,OU=Users,DC=company,DC=net      dn
+    .PARAMETER InputString
+        String to decode as UTF8
+    .OUTPUTS
+        [System.String](https://learn.microsoft.com/en-us/dotnet/api/system.string)
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,Position=0)]
+        [String]$InputString
+    )
+    begin {
+        Write-Verbose "$($MyInvocation.MyCommand) start"
+    }
+    process {
+        $enc = [System.Text.Encoding]::UTF8
+        [System.Text.Encoding]::GetEncoding($InputString)
+        try {
+            $stringBytes = $enc.GetBytes($InputString)
+        } catch {
+            throw $_
+        }
+        try {
+            [System.Text.Encoding]::UTF8.GetString($stringBytes)
+        } catch {
+            throw $_
+        }
+    }
+    end {
+        Write-Verbose "$($MyInvocation.MyCommand) end"
+    }
+}
