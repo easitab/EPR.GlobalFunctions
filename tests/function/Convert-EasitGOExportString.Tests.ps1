3b7a453e3495fd19001128c96e6f8ae29fcd5898
Easit AB <github@easit.com>
2023-10-13 10:21:07 +0200

    add more tests

--- tests/function/Convert-EasitGOExportString.Tests.ps1
+++ tests/function/Convert-EasitGOExportString.Tests.ps1
@@ -30,7 +30,7 @@ BeforeAll {
         }
     }
     $exportString = Get-Content -Path (Join-Path -Path $envSettings.TestDataDirectory -ChildPath 'exportExample.json') -Raw
-    $base64String = 'eyJpbXBvcnRoYW5kbGVyIjoibXlTY3JpcHQucHMxIiwiaXRlbVRvSW1wb3J0Ijp7InByb3BlcnR5IjpbeyJwcm9wZXJ0eTEiOiJzcGVjaWFsQ2hhcmFjdGVycyJ9XX19'
+    $base64String = Get-Content -Path (Join-Path -Path $envSettings.TestDataDirectory -ChildPath 'exportExampleBase64.txt') -Raw
 }
 Describe "Convert-EasitGOExportString" -Tag 'function','public' {
     It 'should have a parameter named InputString that accepts a string' {
@@ -38,6 +38,9 @@ Describe "Convert-EasitGOExportString" -Tag 'function','public' {
     }
     It 'should have a parameter named Raw that is a switch' {
         Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Raw -Type [Switch]
+    }
+    It 'should have a parameter named Full that is a switch' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Full -Type [Switch]
     }
     It 'help section should have a SYNOPSIS' {
         ((Get-Help "$($envSettings.CommandName)" -Full).SYNOPSIS).Length | Should -BeGreaterThan 0
@@ -63,7 +66,55 @@ Describe "Convert-EasitGOExportString" -Tag 'function','public' {
     It 'should return a PSCustomObject (json)' {
         Convert-EasitGOExportString -InputString $exportString | Should -BeOfType [PSCustomObject]
     }
+    It '$returnedObject.ObjectId should be 23468 (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).ObjectId | Should -BeExactly 23468
+    }
+    It '$returnedObject.DatabaseId should be 18830:2 (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).DatabaseId | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.PropertyObjects.Count should be 0 (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).PropertyObjects.Count | Should -BeExactly 0
+    }
+    It '$returnedObject.Attachments.Count should be 0 (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).Attachments.Count | Should -BeExactly 0
+    }
+    It '$returnedObject.uid should be 23468 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).uid | Should -BeExactly 23468
+    }
+    It '$returnedObject.id should be 18830:2 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).id | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.property.Count should be 3 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).property.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.attachment.Count should be 1 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).attachment.Count | Should -BeExactly 1
+    }
     It 'should return a PSCustomObject (base64)' {
         Convert-EasitGOExportString -InputString $base64String | Should -BeOfType [PSCustomObject]
+    }
+    It '$returnedObject.ObjectId should be 23468 (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).ObjectId | Should -BeExactly 23468
+    }
+    It '$returnedObject.DatabaseId should be 18830:2 (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).DatabaseId | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.PropertyObjects.Count should be 0 (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).PropertyObjects.Count | Should -BeExactly 0
+    }
+    It '$returnedObject.Attachments.Count should be 0 (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).Attachments.Count | Should -BeExactly 0
+    }
+    It '$returnedObject.uid should be 23468 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).uid | Should -BeExactly 23468
+    }
+    It '$returnedObject.id should be 18830:2 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).id | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.property.Count should be 3 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).property.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.attachment.Count should be 1 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).attachment.Count | Should -BeExactly 1
     }
 }
--- tests/module/Convert-EasitGOExportString.Tests.ps1
+++ tests/module/Convert-EasitGOExportString.Tests.ps1
@@ -23,7 +23,55 @@ Describe "Convert-EasitGOExportString" -Tag 'module' {
     It 'should return a PSCustomObject (json)' {
         Convert-EasitGOExportString -InputString $exportString | Should -BeOfType [PSCustomObject]
     }
+    It '$returnedObject.PropertyObjects.Count should be 3 (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).PropertyObjects.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.assetCollection should be Arbetsstation bärbar avancerad (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).assetCollection | Should -BeExactly 'Arbetsstation bärbar avancerad'
+    }
+    It '$returnedObject.samAccountName should be klåra (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).samAccountName | Should -BeExactly 'klåra'
+    }
+    It '$returnedObject.dn should be CN=testsson\, test,OU=Users,DC=company,DC=com (json)' {
+        (Convert-EasitGOExportString -InputString $exportString).dn | Should -BeExactly 'CN=testsson\, test,OU=Users,DC=company,DC=com'
+    }
+    It '$returnedObject.uid should be 23468 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).uid | Should -BeExactly 23468
+    }
+    It '$returnedObject.id should be 18830:2 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).id | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.property.Count should be 3 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).property.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.attachment.Count should be 1 (json - raw)' {
+        (Convert-EasitGOExportString -InputString $exportString -Raw).attachment.Count | Should -BeExactly 1
+    }
     It 'should return a PSCustomObject (base64)' {
         Convert-EasitGOExportString -InputString $base64String | Should -BeOfType [PSCustomObject]
+    }
+    It '$returnedObject.PropertyObjects.Count should be 3 (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).PropertyObjects.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.assetCollection should be Arbetsstation bärbar avancerad (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).assetCollection | Should -BeExactly 'Arbetsstation bärbar avancerad'
+    }
+    It '$returnedObject.samAccountName should be klåra (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).samAccountName | Should -BeExactly 'klåra'
+    }
+    It '$returnedObject.dn should be CN=testsson\, test,OU=Users,DC=company,DC=com (base64)' {
+        (Convert-EasitGOExportString -InputString $base64String).dn | Should -BeExactly 'CN=testsson\, test,OU=Users,DC=company,DC=com'
+    }
+    It '$returnedObject.uid should be 23468 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).uid | Should -BeExactly 23468
+    }
+    It '$returnedObject.id should be 18830:2 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).id | Should -BeExactly '18830:2'
+    }
+    It '$returnedObject.property.Count should be 3 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).property.Count | Should -BeExactly 3
+    }
+    It '$returnedObject.attachment.Count should be 1 (base64 - raw)' {
+        (Convert-EasitGOExportString -InputString $base64String -Raw).attachment.Count | Should -BeExactly 1
     }
 }
